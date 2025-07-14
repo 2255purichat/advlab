9a6bf749c5faca6cbfc894b2811062d56c0cb275
2255purichat <purichat.p2006@gmail.com>
2025-07-14 15:23:40 +0700

    Update dlyoutube.py

--- dlyoutube.py
+++ dlyoutube.py
@@ -1,12 +1,12 @@
 # pip install yt_dlp
 
 import yt_dlp
-def download_youtube_video(url, save_path="."):
+def download_youtube_video(url, save_path="."):#ฟังก์ชัน
     ydl_opts = {
-        'outtmpl': f'{save_path}/%(title)s.%(ext)s',
+        'outtmpl': f'{save_path}/%(title)s.%(ext)s',#ใส่ชื่อตัวแปร
         'format': 'best'
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-video_url = input("Enter the YouTube video URL: ")
-download_youtube_video(video_url, save_path=".") #
+video_url = input("Enter the YouTube video URL: ")#ใส่ลิ้งค์ยูทูป
+download_youtube_video(video_url, save_path=".") #้รียกใช้ฟังก์ชัน
